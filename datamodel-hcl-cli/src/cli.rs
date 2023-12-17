fb8ddcbfd0e4ff27861ed7e897119ead9147320f
gNahcab <gregor.bachmann@dasch.swiss>
2023-12-17 15:53:42 +0100

    add: parsing from cli (ongoing)

--- datamodel-hcl-cli/src/cli.rs
+++ datamodel-hcl-cli/src/cli.rs
@@ -3,24 +3,40 @@ use clap::{CommandFactory, Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+
 pub struct Cli {
     /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
+    #[arg(short, long, action = clap::ArgAction::Count, default_value_t = 1)]
     debug: u8,
 
     ///takes an argument
     #[command(subcommand)]
     command: Option<Commands>,
+
+
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// Parses an HCL-Datamodel
-    Import {
+    /// Parses a HCL-Datamodel
+    VALIDATE {
             /// the required path to the datamodel in hcl
-            #[arg(short, long, value_name = "FILE")]
+            #[arg(short, long, value_name = "PROJECT", value_name = "TYPE")]
             project: PathBuf,
+            type_: String,
     },
+    CSV {
+        #[arg(short, long, value_name = "FILE DM", value_name = "FILE TF", value_name = "FILE DT")]
+        project: PathBuf,
+        transform: PathBuf,
+        data: PathBuf,
+    },
+    XLSX {
+        #[arg(short, long, value_name = "FILE DM", value_name = "FILE TF", value_name = "FILE DT")]
+        project: PathBuf,
+        transform: PathBuf,
+        data: PathBuf,
+    }
 }
 pub fn read_in() -> () {
     let cli = Cli::parse();
@@ -35,9 +51,18 @@ pub fn read_in() -> () {
 // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Some(Commands::Import { project}) => (),
+        Some(Commands::VALIDATE { project, type_}) => (
+            // in CLion: '-- validate --project datamodel.hcl datamodel'
+            println!("validate: project {:?}, file_type: {:?}", project, type_)
+            ),
+        Some(Commands::CSV {project, transform, data}) => (
+            println!("Csv: project {:?}, transform: {:?}, data: {:?}", project, transform , data)
+            ),
+        Some(Commands::XLSX { project: project, transform, data}) => (
+            println!("Xlsx: project {:?}, transform: {:?}, data: {:?}", project, transform, data)
+        ),
         // Todo: return message "only the following commands have an effect: 'Import'" ?
-        None => ()
+        None => (println!("none: {:?}", cli.command)),
     }
 
 }
--- datamodel-hcl-cli/src/main.rs
+++ datamodel-hcl-cli/src/main.rs
@@ -4,7 +4,6 @@ use std::env::args;
 use clap::Parser;
 use crate::cli::{Cli, read_in};
 fn main() {
-
     let cli = read_in();
     println!("cli: {:?}", cli);
     dbg!(cli);
